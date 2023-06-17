44679384b15965d14f868de76a08fade798eda5f
dashikapv <67821083+dashikapv@users.noreply.github.com>
2023-06-17 20:54:16 +0530

    Finding average program

---
+++ Avg.java
@@ -0,0 +1,19 @@
+public class Avg
+{
+	 public static void main(String[] args) {
+		{
+
+
+			int mark1 = 90;
+			int mark2 = 80;
+			int mark3 = 70;
+
+			int total = mark1+mark2+mark3;
+			System.out.println("Total Mark is : " + total);
+
+			int average = total/3;
+			System.out.print("Avereage mark is : " + average);
+
+		}
+	}
+}
